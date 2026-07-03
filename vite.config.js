a05e611dfecd3e910569e4039871894114a6e4f3
nuffer1530 <brandyn.nuffer@awesomeservice.com>
2026-07-03 08:20:48 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 3000 },
-  preview: { port: process.env.PORT || 4173, host: true }
+  preview: {
+    port: parseInt(process.env.PORT) || 4173,
+    host: '0.0.0.0'
+  }
 })
